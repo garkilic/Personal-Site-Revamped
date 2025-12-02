ddb5e22ddec8ab4745bc7d76bda6bf6c5f488fee
Griffin Arkilic <garkilic@gmail.com>
2025-12-02 11:11:49 -0800

    fix pt 2

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	// Vercel will automatically detect Next.js projects
+	// No additional configuration needed for basic setup
+};
+
+module.exports = nextConfig;
+
