bfbbee3aeba89a1139bad0379970d70d656c3059
Griffin Arkilic <garkilic@gmail.com>
2025-12-02 11:14:27 -0800

    removed vercel analytics

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-	return (
-		<div>
-			<h1>Welcome</h1>
-			<p>Next.js app is running</p>
-		</div>
-	);
-}
-
--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	// Vercel will automatically detect Next.js projects
-	// No additional configuration needed for basic setup
-};
-
-module.exports = nextConfig;
-
